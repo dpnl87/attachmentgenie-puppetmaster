1b0990c6480cf40b00c92f4051614129084aac9a
unknown <mvandeven@HS-FX-WS-002.HS.LOCAL>
2015-10-19 14:19:51 +0200

    enable tftp/dhcp with foreman

--- manifests/profile_foreman_proxy.pp
+++ manifests/profile_foreman_proxy.pp
@@ -9,11 +9,11 @@ class puppetmaster::profile_foreman_proxy (
     foreman_base_url => "${protocol}://${foreman_host}",
     trusted_hosts    => [$::fqdn, $foreman_host],
     bmc              => false,
-    dhcp             => false,
+    dhcp             => true,
     dns              => false,
     puppetca         => $puppetca,
     puppetrun        => $puppetrun,
-    tftp             => false,
+    tftp             => true,
     version          => $version,
   }
 }
